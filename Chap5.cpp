0bc2ca1567fdb6e0f3c1cc4fa98db50d85ccbb85
AAddison <audrey.smith@aggiemail.usu.edu>
2013-10-14 20:18:12 -0600

    Added a file.

---
+++ Chap5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+// List Function prototypes
+int addition(int a, int b);
+
+
+// Or, include entire function above your "main" function
+void printmessage()
+{
+	cout << "I'm a function!";
+}
+
+
+int main()  // main is a function that takes no inputs and returns an int, it returns 0 which is specified at the bottom
+{
+	// use addition function
+	int a = 5;
+	int b = 3;
+	int z;
+	z = addition(a,b);
+
+	// call printmessage function
+	printmessage();
+
+
+	cin.get();
+	cin.get();
+	return 0;
+}
+
+
+
+int addition(int a, int b)
+{
+	int r = a+b;
+	cout << "The sum of " << a << " and " << b << " is " << r << ".\n";
+	return(r);
+}
